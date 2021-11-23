48a42ea87cf2574c4a18343d4512166cc1f57710
Longteng Zhang <longteng.zhang@tudublin.ie>
2021-11-23 16:27:08 +0000

    refactor: practical 4 - created functions
    Created deg2rad and area_under_tan function

--- practical/04/tancurve.f90
+++ practical/04/tancurve.f90
@@ -1,38 +1,32 @@
-program trapezodial
+program tancurve
     implicit none
     real(8), parameter :: pi = 3.141592653
-    integer, parameter :: pts_num = 12
-    real(8), dimension(0:pts_num) :: pts
-    real(8) :: a, b, sumab, logresult, sumtrape
-    integer :: i
+
+    write(*, *) area_under_tan(1200)
+
+    contains
+    function deg2rad(d) result(r)
+        real(8), intent(in) :: d
+        real(8) :: r
+        r = d * pi / 180.0
+    end function deg2rad
+
+    function area_under_tan(pts_num) result(area)
+        integer, intent(in) :: pts_num
+        real(8), dimension(0:pts_num) :: pts
+        real(8), parameter :: a = 0.0, b = 60.0
+        real(8) :: area
+        integer :: i
     
-    a = 0
-    b = pi / 3.0
+        do, i = 0, pts_num
+            pts(i) = 2 * tan(deg2rad(i * (b - a) / pts_num))
+        end do
+        
+        pts(0) = pts(0) / 2.0
+        pts(pts_num) = pts(pts_num) / 2.0
+        
+        area = deg2rad(b - a) * sum(pts) / 2.0 / pts_num
 
-    sumab = tan(a) + tan(b)
-    write (*, *) "Sum of tan(a) tab(b)"
-    write (*, *) sumab
+    end function area_under_tan
 
-    do, i = 0, pts_num
-        pts(i) = 2 * tan(i * (b - a) / pts_num)
-    end do
-    
-    pts(0) = pts(0) / 2.0
-    pts(pts_num) = pts(pts_num) / 2.0
-    
-    sumtrape = (b - a) * sum(pts) / 2.0 / pts_num
-    logresult = log(2.0)
-
-    write (*, *) "Area by approximation"
-    write (*, *) sumtrape
-    write (*, *) "Area exact result"
-    write (*, *) logresult
-    write (*, *) "Difference"
-    write (*, *) sumtrape - logresult
-
-    ! That's a bit off but reasonable.
-    ! 12 points 1.8e-3
-    ! 1209 points 2.4e-7
-    ! 120000 points 4.6e-8
-
-end program trapezodial
+end program tancurve
