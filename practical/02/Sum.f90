9a5a6034874cd16076e6fa8ccc4cfd754eccab6f
Longteng Zhang <longteng.zhang@tudublin.ie>
2021-11-23 15:04:35 +0000

    feat: completed sum code

--- practical/02/Sum.f90
+++ practical/02/Sum.f90
@@ -8,22 +8,23 @@ program sum
 ! First sum
    sum1 = 0.0
    do i=1,1000
-      !  Insert here
+      sum1 = sum1 + real(1.0 / i)
    end do
 
 
 ! Second sum 
    sum2 = 0.0
    do i=1000,1,-1
-      ! Insert the same line as above except use sum2
+      sum2 = sum2 + real(1.0 / i)
    end do
 
    write(6,*) ' Sum1=',sum1
    write(6,*) ' Sum2=',sum2
 
 ! Find the difference
-   diff = ! ??
+   diff = sum1 - sum2
 
    write(6,*) ' Difference between the two is ',diff
+   ! mathematically the same but errors accumulate differently
 
 end program sum
