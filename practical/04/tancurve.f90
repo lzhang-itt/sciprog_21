886dc48281fe051ca793818ea7d93e9b927c9666
Longteng Zhang <longteng.zhang@tudublin.ie>
2021-11-23 16:38:23 +0000

    fix: change array scope to global (noi really)

--- practical/04/tancurve.f90
+++ practical/04/tancurve.f90
@@ -1,8 +1,9 @@
 program tancurve
     implicit none
     real(8), parameter :: pi = 3.141592653
+    real(8), dimension(:), allocatable :: pts
 
-    write(*, *) area_under_tan(1200)
+    write(*, *) area_under_tan(12)
 
     contains
     function deg2rad(d) result(r)
@@ -13,20 +14,20 @@ program tancurve
 
     function area_under_tan(pts_num) result(area)
         integer, intent(in) :: pts_num
-        real(8), dimension(0:pts_num) :: pts
-        real(8), parameter :: a = 0.0, b = 60.0
+        real(8), parameter :: a = 0.0
+        real(8), parameter :: b = 60.0
         real(8) :: area
         integer :: i
-    
-        do, i = 0, pts_num
-            pts(i) = 2 * tan(deg2rad(i * (b - a) / pts_num))
-        end do
-        
-        pts(0) = pts(0) / 2.0
-        pts(pts_num) = pts(pts_num) / 2.0
-        
-        area = deg2rad(b - a) * sum(pts) / 2.0 / pts_num
 
+        allocate(pts(0:pts_num))
+
+        do, i = 0, pts_num
+            pts(i) = tan(deg2rad(i * (b - a) / pts_num))
+        end do
+
+        pts(1:pts_num-1) = pts(1:pts_num-1) * 2.0 
+        area = deg2rad(b - a) * sum(pts) / 2.0 / pts_num
+        deallocate(pts)
     end function area_under_tan
 
 end program tancurve
